36613cbced712d1e0ef9cc0791a9dd1a746fda33
arafat-rifat <arafat15-12603@diu.edu.bd>
2024-03-23 04:25:15 +0600

    Compliting Split Bill Value From

--- src/Components/FormSplitBill.jsx
+++ src/Components/FormSplitBill.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Button from './Button'
 
 const FormSplitBill = () => {
   return (
    <form className='form-split-bill'> 
      <h2>Split a bill with X</h2>
+
+     <label>💰Bill Value</label>
+     <input type='text'/>
+    <Button> Split bill </Button>
    </form>
   )
 }
